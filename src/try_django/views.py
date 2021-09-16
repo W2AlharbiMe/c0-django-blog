688c1b90142e23cd950ee9bb9eadf8b9d424de9c
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-09-17 00:25:24 +0300

    section2, code

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -5,7 +5,12 @@ def page(request, name, context={}):
  return render(request, 'pages/{0}.jinja2'.format(name), context)
 
 def home_page(request):
-  return page(request, 'home')
+  languages = ["python", "javascript", "php"]
+  context = {
+    "languages": languages
+  }
+
+  return page(request, 'home', context)
 
 def about_page(request):
  return page(request, 'about')
