315e8afcd4dd79e9e6990cf7efb05ed67482cc26
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-09-17 02:21:47 +0300

    section5, unique slugs

--- src/blog/models.py
+++ src/blog/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class BlogPost(models.Model):
   title = models.TextField()
-  slug = models.SlugField()
+  slug = models.SlugField(unique=True)
   content = models.TextField(null=True, blank=True)
