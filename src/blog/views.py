6306785f3c06975a53eae96f78677e2d417b963a
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-09-17 01:57:02 +0300

    section5, better error handling

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from acme.helpers import (page, _render)
 from .models import BlogPost
@@ -7,7 +8,11 @@ from .models import BlogPost
 
 # /blog/<int:id> ex. http://localhost/blog/1
 def blog_post_details_page(request, post_id):
-  blogPosts = BlogPost.objects.get(id=post_id)
+  try:
+    blogPosts = BlogPost.objects.get(id=post_id)
+  except:
+    raise Http404
+
   context = {"post": blogPosts}
 
   return _render(request, 'blog_posts.index', context)
