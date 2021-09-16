a06edf4dbebc539b48fcb0a73450ebcd1fbc7b3c
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-09-16 23:13:23 +0300

    use jinja2 template engine instead of django template language

--- src/try_django/settings.py
+++ src/try_django/settings.py
@@ -52,9 +52,25 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'try_django.urls'
 
 TEMPLATES = [
+    # 3rd party rendering engine to render our pages 
+    {
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+
+    # DTL used to render django admin
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
