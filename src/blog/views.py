3b4db71fc468ab02dd957ea951e291279c977f2e
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-09-17 02:02:31 +0300

    section5, error handling in single line �

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,5 @@
 from django.http.response import Http404
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 from acme.helpers import (page, _render)
 from .models import BlogPost
 
@@ -8,13 +8,7 @@ from .models import BlogPost
 
 # /blog/<int:id> ex. http://localhost/blog/1
 def blog_post_details_page(request, post_id):
-  try:
-    blogPosts = BlogPost.objects.get(id=post_id)
-  except ValueError:
-    raise Http404
-  except:
-    raise Http404
-
-  context = {"post": blogPosts}
+  post = get_list_or_404(BlogPost, id=post_id)
+  context = {"post": post}
 
   return _render(request, 'blog_posts.index', context)
