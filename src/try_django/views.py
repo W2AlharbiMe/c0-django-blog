875d9d9a61d5878347ea545ce41d1c838ad5041d
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-09-16 22:32:11 +0300

    section1, first template

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def home_page(request):
-  return HttpResponse("<h1>Hello, World!</h1>")
+  return render(request, 'home.html')
 
 
 def about_page(request):
