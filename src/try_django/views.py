9435f070f851d73d1bb913a83d04b59d663650e2
W2AlharbiMe <w2.alharbi.me@gmail.com>
2021-09-16 22:23:14 +0300

    multiple views

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -3,3 +3,10 @@ from django.http import HttpResponse
 
 def home_page(request):
   return HttpResponse("<h1>Hello, World!</h1>")
+
+
+def about_page(request):
+ return HttpResponse("<h1>About Page</h1>")
+
+def contact_page(request):
+ return HttpResponse("<h1>Contact Page</h1>")
